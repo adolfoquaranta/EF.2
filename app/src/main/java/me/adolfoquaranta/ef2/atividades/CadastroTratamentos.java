9b708c90745a85a1541284a589b29e80e47938b2
Adolfo Quaranta <adolfo_dtna@hotmail.com>
2017-01-19 23:57:09 -0200

    Atualizações no novo método de criação de lista com campos para receber nomes dos tratamentos

--- app/src/main/java/me/adolfoquaranta/ef2/atividades/CadastroTratamentos.java
+++ app/src/main/java/me/adolfoquaranta/ef2/atividades/CadastroTratamentos.java
@@ -1,42 +1,26 @@
 package me.adolfoquaranta.ef2.atividades;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.design.widget.TextInputEditText;
-import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.util.Log;
-import android.util.Size;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.BaseAdapter;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.ListView;
-import android.widget.Spinner;
-
+import fr.ganfra.materialspinner.MaterialSpinner;
 import me.adolfoquaranta.ef2.R;
 import me.adolfoquaranta.ef2.auxiliares.DBAuxilar;
 import me.adolfoquaranta.ef2.modelos.DIC;
-import me.adolfoquaranta.ef2.modelos.Tratamento;
-
 import com.rengwuxian.materialedittext.MaterialEditText;
 
+
+
 public class CadastroTratamentos extends AppCompatActivity {
-    private ListView list_viewTratamentos;
-    //private TratamentosListAdapter tratamentosListAdapter;
-    private DIC dic;
-    private String[] nomeTratamentosLista;
-    private Integer[] tipoTratamentosLista;
 
 
     @Override
@@ -62,21 +46,92 @@ public class CadastroTratamentos extends AppCompatActivity {
 
         DBAuxilar dbauxiliar = new DBAuxilar(getApplicationContext());
 
-        dic = dbauxiliar.lerDICdoFormulario(idFormulario_DIC);
+        DIC dic = dbauxiliar.lerDICdoFormulario(idFormulario_DIC);
 
         LinearLayout myLayout = (LinearLayout) findViewById(R.id.ll_Tratamentos);
+        final Integer AuxiliarID = dic.getQuantidadeTratamentos_DIC();
+
+        final float scale = getResources().getDisplayMetrics().density;
+        int padding_5dp = (int) (5 * scale + 0.5f);
+
+        String[] opcoesSpinnerTipoTratamento = getResources().getStringArray(R.array.spinnerArray_tipoTratamento);
+
+
+        View.OnFocusChangeListener validar = new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(v instanceof MaterialEditText){
+                    MaterialEditText editText = (MaterialEditText) v;
+                    editText.validate("^(?!\\s*$).+","Insira um nome");
+                    if(v.hasFocus()){
+                        editText.clearValidators();
+                    }
+                }
+                if(v instanceof MaterialSpinner){
+                    MaterialSpinner spinner = (MaterialSpinner) v;
+                    if(spinner.getSelectedItemPosition()==0){
+                        spinner.setError("Error");
+                    }
+                    else{
+                        spinner.setError(null);
+                    }
+                }
+            }
+        };
 
         for (int i = 0; i < dic.getQuantidadeTratamentos_DIC(); i++) {
-            MaterialEditText myEditText = new MaterialEditText(CadastroTratamentos.this); // Pass it an Activity or Context
-            myEditText.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)); // Pass two args; must be LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT, or an integer pixel value.
-            myEditText.setId(i);
-            myEditText.setFloatingLabel(MaterialEditText.FLOATING_LABEL_HIGHLIGHT);
-            myEditText.setHint(getString(R.string.hint_nomeTratamento)+" "+(i+1));
-            myEditText.setFloatingLabelText(getString(R.string.hint_nomeTratamento) + " " + (i + 1));
-            myEditText.setFloatingLabelAnimating(true);
-            myLayout.addView(myEditText);
+            LinearLayout layoutInterno= new LinearLayout(CadastroTratamentos.this);
+            layoutInterno.setOrientation(LinearLayout.HORIZONTAL);
+            layoutInterno.setWeightSum(3);
+
+            //nome tratamento
+            final MaterialEditText etNomeTratamento = new MaterialEditText(CadastroTratamentos.this); // Pass it an Activity or Context
+            etNomeTratamento.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT, 1f)); // Pass two args; must be LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT, or an integer pixel value.
+            etNomeTratamento.setPaddings(0,padding_5dp,0,0);
+            etNomeTratamento.setId(i);
+            etNomeTratamento.setOnFocusChangeListener(validar);
+            etNomeTratamento.setFloatingLabel(MaterialEditText.FLOATING_LABEL_HIGHLIGHT);
+            etNomeTratamento.setHint(getString(R.string.hint_nomeTratamento)+" "+(i+1));
+            etNomeTratamento.setFloatingLabelText(getString(R.string.hint_nomeTratamento) + " " + (i + 1));
+            etNomeTratamento.setFloatingLabelAnimating(true);
+            layoutInterno.addView(etNomeTratamento);
+
+            //tipo tratamento
+            MaterialSpinner spTipoTratamento = new MaterialSpinner(this);
+            ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, opcoesSpinnerTipoTratamento);
+            spTipoTratamento.setAdapter(spinnerArrayAdapter);
+            spTipoTratamento.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT, 2f)); // Pass two args; must be LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT, or an integer pixel value.
+            spTipoTratamento.setId((i+AuxiliarID));
+            spTipoTratamento.setOnFocusChangeListener(validar);
+            layoutInterno.addView(spTipoTratamento);
+            myLayout.addView(layoutInterno);
+
         }
+
+
+
+        Button btnSalvar_Tratamentos = (Button) findViewById(R.id.btn_salvar_Tratamentos);
+
+        btnSalvar_Tratamentos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                for(int i=0; i<AuxiliarID; i++){
+                    MaterialEditText met = (MaterialEditText) findViewById(i);
+                    Log.d("Nome Trat"+(i+1), met.getText().toString());
+                    MaterialSpinner sp = (MaterialSpinner) findViewById((i+AuxiliarID));
+                    Log.d("Tipo Trat"+(i+1), String.valueOf(sp.getSelectedItemPosition()));
+                }
+            }
+        });
+
+
+
+
+
     }
+
+
+
 
 }
 
